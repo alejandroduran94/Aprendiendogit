4f51ce35550b15bdfa361d879c91aa66e12b1436
alejandroduran94 <fredyadr@gmail.com>
2025-04-09 22:19:47 -0500

    prueba sql

---
+++ persona.sql
@@ -0,0 +1,7 @@
+CREATE TABLE persona (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    telefono VARCHAR(20),
+    correo_electronico VARCHAR(100)
+);
