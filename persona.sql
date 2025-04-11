e675d7c5b1183610d5eea05f47b1da0f6fef43a6
alejandroduran94 <fredyadr@gmail.com>
2025-04-10 23:29:55 -0500

    agregar-estado

--- persona.sql
+++ persona.sql
@@ -3,5 +3,6 @@ CREATE TABLE persona (
     nombre VARCHAR(50),
     apellido VARCHAR(50),
     telefono VARCHAR(20),
-    correo_electronico VARCHAR(100)
+    correo_electronico VARCHAR(100),
+    Estado Varchar(10) Default 'Activo'
 );
