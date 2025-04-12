f04d60a554ff644baf3f5161502fd6237a5d4174
alejandroduran94 <fredyadr@gmail.com>
2025-04-11 21:19:49 -0500

    ajuste

--- persona.sql
+++ persona.sql
@@ -1,9 +1,9 @@
 CREATE TABLE persona (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(50),
-    apellido VARCHAR(50),
+    nombres VARCHAR(50),
+    apellidos VARCHAR(50),
     telefono VARCHAR(20),
     correo_electronico VARCHAR(100),
     Estado Varchar(10) Default 'Activo',
-    Edad Int(10)
+    Edad Int(10),
 );
